729c98d678c7cffbc0384de74349823e19aa6831
Nilay Bostan <83247740+nilaybostan@users.noreply.github.com>
2025-09-26 23:19:37 +0300

    Update runTestmacro_data.C

--- runTestmacro_data.C
+++ runTestmacro_data.C
@@ -1,6 +1,6 @@
 #include "testmacro_FullCorrections_NoJets.C"  
 
-void runTestmacro() {
+void runTestmacro_data() {
     std::vector<std::string> files = {
         "root://xrootd-cms.infn.it//store/data/Run2022F/Muon/NANOAOD/22Sep2023-v2/2550000/187620f5-a4ae-467a-a56f-4569e7b32801.root"
     };
