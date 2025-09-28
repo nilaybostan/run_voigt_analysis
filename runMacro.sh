1eee2bc40ce6cacc77d82f61a8acbfaf6d498be7
Nilay Bostan <83247740+nilaybostan@users.noreply.github.com>
2025-09-28 18:46:41 +0300

    Update runMacro.sh

--- runMacro.sh
+++ runMacro.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 # runMacro.sh
+
+# Set up CMSSW environment
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cd /afs/cern.ch/user/n/nbostan/new_CMS/CMSSW_14_0_18/src
 eval `scramv1 runtime -sh`
 cd RoccoR
 
-root -l -b -q runTestMacro.C
+# Make sure output folder exists
+mkdir -p output
+
+# Run your ROOT macro
+root -l -b -q 'testmacro_FullCorrections_NoJets_2.C+'
+
+# Move all PDFs into output/
+mv *.pdf output/ 2>/dev/null || true
