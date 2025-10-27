5002f68ba081777a3351f300b58d5424a778fdca
Nilay Bostan <83247740+nilaybostan@users.noreply.github.com>
2025-10-27 11:22:46 +0300

    Update train_BDT_HTo2Mu.C

--- train_BDT_HTo2Mu.C
+++ train_BDT_HTo2Mu.C
@@ -6,6 +6,8 @@
 #include "TMVA/DataLoader.h"
 #include "TMVA/Tools.h"
 #include "TMVA/TMVAGui.h"
+#include "TCanvas.h"
+#include "TGraph.h"
 
 // how to run: 
 // root -l
@@ -27,9 +29,9 @@ void train_BDT_HTo2Mu() {
     TMVA::DataLoader* dataloader = new TMVA::DataLoader("dataset");
 
     // --------------------------------------------------------------
-    // Sinyal (H→μμ) ve background (Z→μμ, DY, ttbar, vb.) dosyalarını aç
+    // Sinyal ve background dosyalarını aç
     // --------------------------------------------------------------
-    TFile* inputSig = TFile::Open("root://xrootd-cms.infn.it//store/mc/Run3Summer22NanoAODv12/VBFHto2Mu_M-125_TuneCP5_withDipoleRecoil_13p6TeV_powheg-pythia8/NANOAODSIM/130X_mcRun3_2022_realistic_v5-v3/50000/07efcc80-1811-4648-90bb-9a52a423a65f.root");
+    TFile* inputSig = TFile::Open("root://xrootd-cms.infn.it//store/mc/Run3Summer22NanoAODv12/VBFHto2Mu_M-125_TuneCP5_withDipoleRecoil_13p6TeV_powheg-pythia8/NANOAODSIM/130X_mcRun3_2022_realistic_v5-v3/50000/0ea52cdc-3d79-497f-9275-b6fd2041e9a0.root");
     TFile* inputBkg = TFile::Open("root://xrootd-cms.infn.it//store/mc/Run3Summer22NanoAODv12/DYto2L-2Jets_MLL-50_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/NANOAODSIM/130X_mcRun3_2022_realistic_v5-v2/2520000/aec6684e-3416-4b7c-a96f-78f261659521.root");
 
     TTree* signalTree = (TTree*)inputSig->Get("Events");
@@ -39,50 +41,40 @@ void train_BDT_HTo2Mu() {
     dataloader->AddBackgroundTree(backgroundTree, 1.0);
 
     // --------------------------------------------------------------
-    // Kullanılacak değişkenleri tanımla (NanoAOD uyumlu)
+    // Değişkenler (Alt$() ile güvenli)
     // --------------------------------------------------------------
 
-    // m(μμ)
     dataloader->AddVariable(
-        "sqrt(2*Muon_pt[0]*Muon_pt[1]*(cosh(Muon_eta[0]-Muon_eta[1]) - cos(Muon_phi[0]-Muon_phi[1])))",
+        "sqrt(2*Alt$(Muon_pt[0],0)*Alt$(Muon_pt[1],0)*(cosh(Alt$(Muon_eta[0],0)-Alt$(Muon_eta[1],0)) - cos(Alt$(Muon_phi[0],0)-Alt$(Muon_phi[1],0))))",
         "dimuon_mass", "GeV", 'F');
 
-    // pT(μμ)
     dataloader->AddVariable(
-        "sqrt( pow(Muon_pt[0]*cos(Muon_phi[0]) + Muon_pt[1]*cos(Muon_phi[1]), 2) + "
-        "pow(Muon_pt[0]*sin(Muon_phi[0]) + Muon_pt[1]*sin(Muon_phi[1]), 2) )",
+        "sqrt(pow(Alt$(Muon_pt[0],0)*cos(Alt$(Muon_phi[0],0)) + Alt$(Muon_pt[1],0)*cos(Alt$(Muon_phi[1],0)),2) + "
+        "pow(Alt$(Muon_pt[0],0)*sin(Alt$(Muon_phi[0],0)) + Alt$(Muon_pt[1],0)*sin(Alt$(Muon_phi[1],0)),2))",
         "dimuon_pt", "GeV", 'F');
 
-    // Tekil muon değişkenleri
-    dataloader->AddVariable("Muon_pt[0]", "muon1_pt", "GeV", 'F');
-    dataloader->AddVariable("Muon_pt[1]", "muon2_pt", "GeV", 'F');
-    dataloader->AddVariable("Muon_eta[0]", "muon1_eta", "", 'F');
-    dataloader->AddVariable("Muon_eta[1]", "muon2_eta", "", 'F');
+    dataloader->AddVariable("Alt$(Muon_pt[0],0)", "muon1_pt", "GeV", 'F');
+    dataloader->AddVariable("Alt$(Muon_pt[1],0)", "muon2_pt", "GeV", 'F');
+    dataloader->AddVariable("Alt$(Muon_eta[0],0)", "muon1_eta", "", 'F');
+    dataloader->AddVariable("Alt$(Muon_eta[1],0)", "muon2_eta", "", 'F');
 
-    // ΔR(μ, μ)
     dataloader->AddVariable(
-        "sqrt( pow(Muon_eta[0]-Muon_eta[1],2) + pow(acos(cos(Muon_phi[0]-Muon_phi[1])),2) )",
+        "sqrt(pow(Alt$(Muon_eta[0],0)-Alt$(Muon_eta[1],0),2)+pow(acos(cos(Alt$(Muon_phi[0],0)-Alt$(Muon_phi[1],0))),2))",
         "deltaR_mumu", "", 'F');
 
-    // Jet sayısı
-    dataloader->AddVariable("nJet", "nJets", "", 'I');
-
-    // Missing ET (isteğe bağlı)
-    dataloader->AddVariable("MET_pt", "met", "GeV", 'F');
+    dataloader->AddVariable("Alt$(nJet,0)", "nJets", "", 'I');
+    dataloader->AddVariable("Alt$(MET_pt,0)", "met", "GeV", 'F');
 
     // --------------------------------------------------------------
-    // Basit olay seçimi (preselection)
+    // Preselection
     // --------------------------------------------------------------
-    //
-    // TMVA ROOT’un TCut sınıfını kullanır. TString yerine TCut olmalı.
-
     TCut preselection =
-        "(nMuon >= 2)"
-        " && (Muon_charge[0] != Muon_charge[1])"
-        " && (Muon_pt[0] > 20) && (Muon_pt[1] > 15)"
-        " && (abs(Muon_eta[0]) < 2.4) && (abs(Muon_eta[1]) < 2.4)"
-        " && (sqrt(2*Muon_pt[0]*Muon_pt[1]*(cosh(Muon_eta[0]-Muon_eta[1]) - cos(Muon_phi[0]-Muon_phi[1]))) > 70)"
-        " && (sqrt(2*Muon_pt[0]*Muon_pt[1]*(cosh(Muon_eta[0]-Muon_eta[1]) - cos(Muon_phi[0]-Muon_phi[1]))) < 115)";
+        "(Alt$(nMuon,0) >= 2)"
+        " && (Alt$(Muon_charge[0],0) != Alt$(Muon_charge[1],0))"
+        " && (Alt$(Muon_pt[0],0) > 20) && (Alt$(Muon_pt[1],0) > 15)"
+        " && (abs(Alt$(Muon_eta[0],0)) < 2.4) && (abs(Alt$(Muon_eta[1],0)) < 2.4)"
+        " && (sqrt(2*Alt$(Muon_pt[0],0)*Alt$(Muon_pt[1],0)*(cosh(Alt$(Muon_eta[0],0)-Alt$(Muon_eta[1],0)) - cos(Alt$(Muon_phi[0],0)-Alt$(Muon_phi[1],0)))) > 70)"
+        " && (sqrt(2*Alt$(Muon_pt[0],0)*Alt$(Muon_pt[1],0)*(cosh(Alt$(Muon_eta[0],0)-Alt$(Muon_eta[1],0)) - cos(Alt$(Muon_phi[0],0)-Alt$(Muon_phi[1],0)))) < 115)";
 
     // --------------------------------------------------------------
     // Eğitim / test ayrımı
@@ -93,7 +85,7 @@ void train_BDT_HTo2Mu() {
     );
 
     // --------------------------------------------------------------
-    // BDT yöntemi (Boosted Decision Trees)
+    // BDT yöntemi
     // --------------------------------------------------------------
     factory.BookMethod(dataloader, TMVA::Types::kBDT, "BDT",
         "!H:!V:NTrees=500:MaxDepth=3:BoostType=Grad:"
@@ -107,7 +99,21 @@ void train_BDT_HTo2Mu() {
     factory.EvaluateAllMethods();
 
     // --------------------------------------------------------------
-    // Çıktı ve ROC GUI
+    // ROC eğrisi çizimi
+    // --------------------------------------------------------------
+    TFile* resultsFile = TFile::Open(outfileName);
+    TKey* key = (TKey*)resultsFile->GetListOfKeys()->FindObject("dataset/Method_BDT/MVA_BDT");
+    if (key) {
+        TH1* hROC = (TH1*)resultsFile->Get("dataset/Method_BDT/MVA_BDT/MVA_BDT_TrainingROC");
+        if (hROC) {
+            TCanvas* cROC = new TCanvas("cROC", "ROC Curve", 800, 600);
+            hROC->Draw("AL");
+            cROC->SaveAs("ROC_curve.png");
+        }
+    }
+
+    // --------------------------------------------------------------
+    // Çıktıyı kapat
     // --------------------------------------------------------------
     outputFile->Close();
     std::cout << "BDT eğitimi tamamlandı! Sonuç dosyası: " << outfileName << std::endl;
